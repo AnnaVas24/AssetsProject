beeb54340a9dc73da5712aeddfc791704f8d684a
Anna <anna.vasi4ko@icloud.com>
2022-06-23 14:03:26 +0300

    Initial Commit

---
+++ AssetsProject/AssetsProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssetsProjectApp.swift
+//  AssetsProject
+//
+//  Created by Vasichko Anna on 23.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct AssetsProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
